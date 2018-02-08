f09633e943cb20b71aae9f4a12801348ebc1dd70
emhopkins <epoletto@lstreetc.com>
2018-02-07 22:03:24 -0600

    get next functionality for characteristics show

--- app/controllers/characteristics_controller.rb
+++ app/controllers/characteristics_controller.rb
@@ -2,7 +2,7 @@ class CharacteristicsController < ApplicationController
 	def show
 		@characteristic = Characteristic.find(params[:id])
 		respond_to do |format|
-			format.html 
+			format.html { render :show }
 			format.json { render json: @characteristic }
 		end
 	end
