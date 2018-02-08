dc6f8d88b0a260eeb8809fb99658ba42b30bab85
emhopkins <epoletto@lstreetc.com>
2018-02-07 19:55:56 -0600

    clean up code, remove console logs

--- app/assets/javascripts/buildings.js
+++ app/assets/javascripts/buildings.js
@@ -5,9 +5,7 @@ $(document).ready(function () {
 	$.get(url)
 	  .done(function( data ) {
 	  	$('#buildings').html("");
-	    console.log(data);
 	    $.each(data, function(index, value) {
-			  console.log(value["name"]);
 			  var buildingPath = url + '/' + value["id"]
 			  $('#buildings').append(`<div><a href='${buildingPath}'>${value["name"]}</a></div>`)
 			});
