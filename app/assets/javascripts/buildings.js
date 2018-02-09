3c7e5117bc54f2f32e3a657e7a2a00c31c8ad4d8
emhopkins <epoletto@lstreetc.com>
2018-02-08 21:20:17 -0600

    adding apartment serializer and apartment js object to show page

--- app/assets/javascripts/buildings.js
+++ app/assets/javascripts/buildings.js
@@ -7,6 +7,8 @@ $(document).ready(function () {
 	  	$('#buildings').html("");
 	    $.each(data, function(index, value) {
 	    	var newBuilding = new Building(value, url);
+	    	
+	    	console.log(newBuilding)
 	    	var buildingHtml = newBuilding.formatDisplay();
 			  $('#buildings').append(buildingHtml)
 			});
@@ -19,9 +21,33 @@ function Building(building, url) {
 	this.name = building.name
 	this.address = building.address
 	this.buildingUrl = url + '/' + building.id
+	var apartment_objects_array = []
+	$.each(building.apartments, function() {
+		var newApartment = new Apartment(this)
+		apartment_objects_array.push(newApartment);
+	})
+	this.apartments = apartment_objects_array
 };
 
 Building.prototype.formatDisplay = function() {
-	var postHtml = `<div><a href='${this.buildingUrl}'>${this.name}</a> located at address: ${this.address}</div>`;
-	return postHtml;
+	var apartmentsHtml = ""
+	$.each(this.apartments, function() {
+		var apartmentListItem = this.formatDisplay()
+		apartmentsHtml += apartmentListItem
+	});
+	var buildingHtml = `<div><a href='${this.buildingUrl}'>${this.name}</a> located at address: ${this.address}</div>
+											<ul>${apartmentsHtml}</ul>`;
+	return buildingHtml;
+};
+
+function Apartment(apartment) {
+	this.id = apartment.id
+	this.unit = apartment.unit
+	this.description = apartment.description
+};
+
+Apartment.prototype.formatDisplay = function() {
+	console.log(this)
+	var apartmentHtml = `<li>${this.unit}</li>`
+	return apartmentHtml;
 };
--- app/serializers/building_serializer.rb
+++ app/serializers/building_serializer.rb
@@ -1,3 +1,4 @@
 class BuildingSerializer < ActiveModel::Serializer
   attributes :id, :name, :address
+  has_many :apartments
 end
